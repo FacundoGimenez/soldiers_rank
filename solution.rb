d9cf563914f1a674b10fd1767ed39496f869a32b
Facundo <faacuu.g@gmail.com>
2020-03-03 19:16:52 -0300

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,12 +1,12 @@
-def get_reporting_rank(ranks_soldiers)
-  length = ranks_soldiers.count
+def solution(ranks)
+  length = ranks.count
   reporting_soldier = 0
   next_superior_reporting = 0
   i = 0
-  ranks_soldiers.sort!
-  ranks_soldiers.each do |rank|
+  ranks.sort!
+  ranks.each do |rank|
     upper = rank + 1
-    if ranks_soldiers.include?(upper)
+    if ranks.include?(upper)
       if i != length
        reporting_soldier = reporting_soldier + 1
      end
